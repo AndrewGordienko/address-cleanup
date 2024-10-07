1ab8ebc4c14371d142fc727ebe4f14e61918c788
Andrew <60705784+AndrewGordienko@users.noreply.github.com>
2024-10-06 21:12:57 -0400

    Update example.py

--- example.py
+++ example.py
@@ -1,9 +1,8 @@
 from llama_cleanup.main import AddressLookup
 
-# Initialize the class and use it
 address_lookup = AddressLookup(
-    canadian_postal_codes_path="CanadianPostalCodes202403.csv",
-    us_zip_codes_path="USZIPCodes202409.csv",
+    canadian_postal_codes_path="/path/to/CanadianPostalCodes202403.csv",
+    us_zip_codes_path="/path/to/USZIPCodes202409.csv",
     llama_model="llama3.1"
 )
 
@@ -14,4 +13,21 @@ print(result)
 """
 Example Output:
 {'city': 'Toronto', 'state_full': 'Ontario', 'latitude': np.float64(43.688438), 'longitude': np.float64(-79.307762), 'country': 'Canada'}
+"""
+
+"""
+# For a remote model:
+
+address_lookup = AddressLookup(
+    canadian_postal_codes_path="/path/to/CanadianPostalCodes202403.csv",
+    us_zip_codes_path="/path/to/USZIPCodes202409.csv",
+    llama_model="gemma:2b",  # The model on the remote server
+    remote=True,  # Enable remote
+    remote_api_base="http://192.168.0.15:11434/v1",  # Remote API base URL
+    remote_api_key="your_api_key_here"  # Optional API key for authentication
+)
+
+result = address_lookup.lookup("456 Pine St New York, NY, USA")
+print(result)
+
 """
